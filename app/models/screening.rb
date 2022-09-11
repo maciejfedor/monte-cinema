72a06aeb6ef3fa807755aa0709a2489a7090cc20
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-12 00:06:05 +0200

    add one-to-many associations between screenings and halls/movies

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -1,2 +1,4 @@
 class Screening < ApplicationRecord
+  belongs_to :movie
+  belongs_to :hall
 end
--- db/migrate/20220911215710_create_screenings.rb
+++ db/migrate/20220911215710_create_screenings.rb
@@ -3,6 +3,8 @@ class CreateScreenings < ActiveRecord::Migration[7.0]
     create_table :screenings do |t|
       t.datetime :start_time
       t.datetime :end_time
+      t.belongs_to :movie
+      t.belongs_to :hall
       t.timestamps
     end
   end
