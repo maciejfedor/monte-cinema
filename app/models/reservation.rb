61138ccdd16bef8673613ef5f3a0867304081dec
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-15 11:26:12 +0200

    add reservation model validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :screening
   has_many :tickets
+  validates :status, presence: true
+  validates :status, inclusion: { in: %w[Booked Cancelled Accepted] }
 end
