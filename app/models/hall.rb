bcca87fb8c1dd0ef00e1b6a11d2cbc288ea331cb
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-15 13:05:25 +0200

    add make  seats method to hall model

--- app/models/hall.rb
+++ app/models/hall.rb
@@ -4,4 +4,8 @@ class Hall < ApplicationRecord
   validates :capacity, numericality: { only_integer: true, greater_than: 1 }
   validates :name, :capacity, presence: true
   validates :name, uniqueness: true
+
+  def make_seats
+    (1..capacity).to_a
+  end
 end
