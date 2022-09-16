4be2b3f335ae6f60f9adb36f94c9dab1d12156c7
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-16 10:27:15 +0200

    update has-many assocciation in reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :screening
-  has_many :tickets
+  has_many :tickets, dependent: :destroy
   validates :status, presence: true
   enum status: { booked: 'booked', confirmed: 'confirmed', canceled: 'canceled' }
 end
