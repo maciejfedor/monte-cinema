b7755bfca5c385a4396662416aa435683c4954f6
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-19 10:50:06 +0200

    add available_seats method to screening model

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -1,7 +1,18 @@
 class Screening < ApplicationRecord
   belongs_to :movie
   belongs_to :hall
+  has_many :reservations
   validates :start_time, :end_time, presence: true
 
   validates :end_time, comparison: { greater_than: :start_time }
+
+  def available_seats
+    taken_seats = []
+    reservations.each do |r|
+      r.tickets.each do |t|
+        taken_seats.push(t.seat)
+      end
+    end
+    hall.seats_array - taken_seats
+  end
 end
