ac8b16ac2227bc20e5b26c3ef29edf2b2536723b
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-19 10:48:32 +0200

    add create action to reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,29 @@
 class ReservationsController < ApplicationController
+  before_action :set_screening, only: %i[new create]
   def new
     @reservation = Reservation.new
+    @screening = Screening.find(params[:screening_id])
+  end
+
+  def create
+    @reservation = Reservation.new(screening_id: params[:screening_id], status: :booked)
+
+    if !params.key?(:seats)
+      render :new, status: :unprocessable_entity
+
+    else
+      @reservation.save
+      params[:seats].each do |seat|
+        Ticket.create(reservation_id: @reservation.id, seat:)
+      end
+      redirect_to movies_path
+
+    end
+  end
+
+  private
+
+  def set_screening
     @screening = Screening.find(params[:screening_id])
   end
 end
