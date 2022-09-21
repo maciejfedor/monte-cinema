c9b2179184d6d0830864c0e29ba51e9726074483
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-21 17:01:04 +0200

    add seat validation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,9 @@
 class Ticket < ApplicationRecord
   belongs_to :reservation
   validates :seat, presence: true
+  validate :seat_available?
+
+  def seat_available?
+    raise StandardError, 'Seat taken' unless Reservation.find(reservation_id).screening.available_seats.include? seat
+  end
 end
