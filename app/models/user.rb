8817d0248433a98b26568b709eba455b8eeab75a
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-22 16:39:41 +0200

    add password bytesize length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+  validate :validate_password_length
+
+  has_many :reservations
+
+  private
+         def validate_password_length
+            errors.add('Password is too long') if password.nil? || password.bytesize > 72
+         end
+    end
