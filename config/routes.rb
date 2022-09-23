a5fd84c36554364ec89c5a98ba516c8f69a8b8d2
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-23 09:49:03 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'movies#index'
+  root 'pages#home'
   resources :movies, only: %i[show index] do
     resources :screenings, only: [:show]
   end
 
   resources :screenings, only: [:show] do
-    resources :reservations, only: %i[new create show update]
+    resources :reservations, only: %i[new create show update destroy]
   end
+
+  resources :reservations, only: %i[index]
 end
