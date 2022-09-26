cbdb400e26d3c6eebc8ebde9c1e4c5ad47dd374b
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-26 23:38:18 +0200

    change user to optional in reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :screening
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :tickets, dependent: :destroy
   validates :status, presence: true
   enum :status, %i[booked cancelled accepted]
