043137032d591fa56d35c64f283cebd4fc64b765
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-27 19:40:56 +0200

    add edit action to movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[:id])
+    set_movie
   end
 
   def new
@@ -12,12 +12,35 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new(title: params[:title], duration: params[:duration])
+    @movie = Movie.new(movie_params)
 
     if @movie.save
-      redirect_to movie_path(@movie)
+      redirect_to @movie
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    set_movie
+  end
+
+  def update
+    set_movie
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :duration, :id)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
   has_many :screenings
-  validates_associated :screenings
   validates :title, :duration, presence: true
   validates :duration, numericality: { greater_than: 0 }
   validates :title, length: { maximum: 200 }
