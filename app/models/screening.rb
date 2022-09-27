6950b135e3ccf32f092d6c3c47959f86f5692cb7
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-27 22:54:25 +0200

    add screening end time calcuation method

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -3,11 +3,15 @@ class Screening < ApplicationRecord
   belongs_to :hall
   has_many :reservations
   validates :start_time, :end_time, presence: true
-
   validates :end_time, comparison: { greater_than: :start_time }
+  before_validation :calculate_end_time
 
   def available_seats
     taken_seats = reservations.joins(:tickets).where.not(status: :cancelled).pluck(:'tickets.seat')
     hall.seats - taken_seats.flatten
+  end
+
+  def calculate_end_time
+    self.end_time = start_time + ADS_DURATION + movie.duration.minutes if start_time.present?
   end
 end
