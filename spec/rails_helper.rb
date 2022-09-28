19e62f4a2cc12d848c87c5f15d3d82ba38effa8d
Maciej Fedor <maciej.fedor@monterail.com>
2022-09-28 13:57:16 +0200

    update rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/app/channels/'
+  add_filter '/app/jobs/'
+  add_filter '/app/mailers/'
+end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -6,12 +12,6 @@ require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'simplecov'
-SimpleCov.start 'rails' do
-  add_filter '/app/channels/'
-  add_filter '/app/jobs/'
-  add_filter '/app/mailers/'
-end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
