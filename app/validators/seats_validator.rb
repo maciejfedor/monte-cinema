5589b85b029ab5636bbdb5f7499b015f2ddf6769
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-03 12:51:39 +0200

    add custom seats validator

---
+++ app/validators/seats_validator.rb
@@ -0,0 +1,15 @@
+class SeatsValidator
+  def self.validate!(screening:, seats:)
+    return false if seats.blank?
+    return false if seat_taken?(screening, seats)
+
+    true
+  end
+end
+
+def seat_taken?(screening, seats)
+  seats.each do |seat|
+    return true unless screening.available_seats.include? seat
+  end
+  false
+end
