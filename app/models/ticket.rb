baf24b39d3ebc683cc848790950e1396396bb768
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-03 12:51:02 +0200

    delete seats validator from ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,9 +1,4 @@
 class Ticket < ApplicationRecord
   belongs_to :reservation
   validates :seat, presence: true
-  validate :seat_available?
-
-  def seat_available?
-    errors.add(seat: 'Seat taken') unless reservation.screening.available_seats.include? seat
-  end
 end
