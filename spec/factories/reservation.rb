12da803f17f6e2be0348c789beda435afe189d0a
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-03 20:15:02 +0200

    add ticket and reservation factory

---
+++ spec/factories/reservation.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :reservation do
+    screening_id {1}
+    status {0}
+  end
+end
---
+++ spec/factories/ticket.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :ticket do
+    seat {'10'}
+  end
+end
