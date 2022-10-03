68126cfe11c03a1359e6e139a0c8a91411c1b5bc
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-03 12:50:09 +0200

    update create usecases

--- app/concepts/reservations/use_cases/create.rb
+++ app/concepts/reservations/use_cases/create.rb
@@ -13,17 +13,21 @@ module Reservations
 
       def call
         ActiveRecord::Base.transaction do
-          repository.create_reservation(screening_id:, user_id:,
-                                        status:).tap do |reservation|
-            create_tickets(reservation)
-          end
+          reservation
+          create_tickets(reservation)
         end
+        reservation
       end
 
       def create_tickets(reservation)
         seats.each do |seat|
           Ticket.create!(seat:, reservation_id: reservation.id)
         end
+      end
+
+      def reservation
+        @reservation ||= repository.create_reservation!(screening_id:, user_id:,
+                                                        status:)
       end
     end
   end
