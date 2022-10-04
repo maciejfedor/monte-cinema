66816fd909cf21984a9f90b5b5c2eeef9968840d
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-04 10:57:49 +0200

    update update and cancel use case tests

--- spec/use_cases/reservations/cancel_spec.rb
+++ spec/use_cases/reservations/cancel_spec.rb
@@ -2,15 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Reservations::UseCases::Cancel do
   describe ".call" do
-    let(:seats) { [10, 11, 12, 13] }
-    let(:user) { create(:user) }
     let(:movie) { create(:movie) }
     let(:hall) { create(:hall) }
-    let(:screening) { create(:screening, hall_id: hall.id, movie_id: movie.id, start_time: Date.current + 2.days) }
-    let(:reservation) { create(:reservation, screening_id: screening.id, status: 0) }
-    let(:params) do { id: reservation.id }
-    
-  end
+    let(:screening) { create(:screening, hall:, movie:, start_time: Date.current + 2.days) }
+    let(:reservation) { create(:reservation, screening:, status: :booked) }
+    let(:params) do 
+      { id: reservation.id }
+    end
 
   subject(:cancel_reservation) { described_class.new(**params).call }
 
