57e8df7d1780eabb520628056f551893250b72bc
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-04 10:08:58 +0200

    change seats validator

--- app/validators/seats_validator.rb
+++ app/validators/seats_validator.rb
@@ -1,14 +1,10 @@
 class SeatsValidator
-  attr_reader :errors
-
-  def self.validate!(screening:, seats:)
-    return false if seats.blank?
-    return false if seat_taken?(screening, seats)
-
-    true
+  def self.validate!(screening:, seats:, errors:)
+    errors.push('Seat must be chosen') if seats.blank?
+    errors.push('Seat taken') if !seats.blank? && seat_taken?(screening, seats)
   end
 
-  def seat_taken?(screening, seats)
+  def self.seat_taken?(screening, seats)
     seats.each do |seat|
       return true unless screening.available_seats.include? seat
     end
