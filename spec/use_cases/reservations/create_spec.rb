3fa93f273373c0ad0826d404eb8ddd11c309bc2d
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-04 14:51:09 +0200

    update create use case tests

--- spec/use_cases/reservations/create_spec.rb
+++ spec/use_cases/reservations/create_spec.rb
@@ -1,58 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Reservations::UseCases::Create do
-  let(:seats) { [10, 11, 12, 13] }
+  let(:seats) { [*'10'..'13'] }
   let(:user) { create(:user) }
   let(:movie) { create(:movie) }
   let(:hall) { create(:hall) }
-  let(:screening) { create(:screening, hall_id: hall.id, movie_id: movie.id, start_time: Date.current + 2.days) }
+  let(:screening) { create(:screening, hall:, movie:, start_time: Date.current + 2.days) }
   let(:params) do
     {
       screening_id: screening.id,
       user_id: user.id,
-      seats:,
-      status: 0
-
+      seats: seats,
+      status: :booked
     }
   end
 
-  let(:instance) { described_class.new(**params) }
+  let(:instance) { described_class.new(**params).call }
 
   describe '.call' do
     it 'creates reservation' do
-      expect { instance.call }.to change(Reservation, :count).by(1)
+      expect{ instance }.to change(Reservation, :count).by(1)
     end
 
     it 'creates tickets' do
-      expect { instance.call }.to change(Ticket, :count).by(4)
-    end
-
-    it 'returns reservation with tickets' do
-      expect(instance.call.tickets.count).to eq(4)
+      expect{ instance }.to change(Ticket, :count).by(4)
     end
 
     it 'returns reservation with proper attributes' do
-      expect(instance.call).to have_attributes(screening_id: screening.id, user_id: user.id, status: 'booked')
+      expect(instance.data).to have_attributes(screening_id: screening.id, user_id: user.id, status: 'booked')
     end
 
     it 'returns reservation without errors' do
-      expect(instance.call.errors).to match_array([])
+      expect(instance.errors).to match_array([])
     end
 
     context 'when seats invalid' do
-      let(:params) do
-        {
-          screening_id: screening.id,
-          user_id: user.id,
-          seats: [],
-          status: 0
-        }
-      end
+      let(:seats) { [] }
+      let(:instance) { described_class.new(**params).call }
       it 'does not create reservation' do
-        expect { instance.call }.not_to change(Reservation, :count)
+        expect { instance }.not_to change(Reservation, :count)
       end
       it 'does not create tickets' do
-        expect { instance.call }.not_to change(Ticket, :count)
+        expect { instance }.not_to change(Ticket, :count)
       end
     end
   end
