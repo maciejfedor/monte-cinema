f54d5e3f2cd46b373cb7603e79c0e9bb177a6d4b
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-05 01:16:18 +0200

    define perform method in cancel reservations job

--- app/jobs/cancel_pending_reservations_job.rb
+++ app/jobs/cancel_pending_reservations_job.rb
@@ -1,7 +1,30 @@
 class CancelPendingReservationsJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(*_args)
+    reservations.each do |reservation|
+      @reservation = reservation
+      cancel_reservation if after_deadline?
+    end
+  end
+
+  private
+
+  attr_reader :reservation
+
+  def reservations
+    Reservation.where(status: :booked)
+  end
+
+  def deadline
+    @reservation.screening.start_time - 30.minutes
+  end
+
+  def after_deadline?
+    Time.current.after?(deadline)
+  end
+
+  def cancel_reservation
+    Reservations::UseCases::Cancel.new(id: @reservation.id).call
   end
 end
