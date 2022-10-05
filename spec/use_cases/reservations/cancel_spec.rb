0ee7e8ee3a868373859d20d249aff5f108fe2be4
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-06 01:05:42 +0200

    fix update and cancel use case tests

--- spec/use_cases/reservations/cancel_spec.rb
+++ spec/use_cases/reservations/cancel_spec.rb
@@ -2,17 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Reservations::UseCases::Cancel do
   describe '.call' do
-    let(:seats) { [*'10'..'13'] }
     let(:movie) { create(:movie) }
     let(:hall) { create(:hall) }
-    let(:user) { create(:user) }
     let(:screening) { create(:screening, hall:, movie:, start_time: Date.current + 2.days) }
-    let(:reservation) do
-      Reservations::UseCases::Create.new(screening_id: screening.id,
-                                         seats:,
-                                         user_id: user.id,
-                                         status: :booked).call
-    end
+    let(:reservation) { create(:reservation, :skip_validate) }
 
     let(:params) do
       {
@@ -20,7 +13,7 @@ RSpec.describe Reservations::UseCases::Cancel do
       }
     end
 
-    let(:cancel_reservation) { described_class.new(**params).call }
+    subject(:cancel_reservation) { described_class.new(**params).call }
 
     it 'cancels reservation' do
       expect { cancel_reservation }.to change { reservation.reload.status }.from('booked').to('cancelled')
--- spec/use_cases/reservations/update_spec.rb
+++ spec/use_cases/reservations/update_spec.rb
@@ -2,17 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Reservations::UseCases::Update do
   describe '.call' do
-    let(:seats) { [*'10'..'13'] }
     let(:movie) { create(:movie) }
     let(:hall) { create(:hall) }
-    let(:user) { create(:user) }
     let(:screening) { create(:screening, hall:, movie:, start_time: Date.current + 2.days) }
-    let(:reservation) do
-      Reservations::UseCases::Create.new(screening_id: screening.id,
-                                         seats:,
-                                         user_id: user.id,
-                                         status: :booked).call
-    end
+    let(:reservation) { create(:reservation, :skip_validate) }
 
     let(:params) do
       {
@@ -22,7 +15,7 @@ RSpec.describe Reservations::UseCases::Update do
       }
     end
 
-    let(:update_reservation) { described_class.new(**params).call }
+    subject(:update_reservation) { described_class.new(**params).call }
 
     it 'updates reservation status' do
       expect { update_reservation }.to change { reservation.reload.status }.from('booked').to('accepted')
