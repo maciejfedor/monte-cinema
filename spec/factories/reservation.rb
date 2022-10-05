df7cce16c9c5277b34d24d6c3aa900c46036c9a1
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-06 00:21:32 +0200

    add skip validate to reservation factory

--- spec/factories/reservation.rb
+++ spec/factories/reservation.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :reservation do
     screening
     status { 'booked' }
+    trait :skip_validate do
+      to_create { |instance| instance.save(validate: false) }
+    end
   end
 end
