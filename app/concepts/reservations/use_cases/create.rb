0415932a1cfef65eca4e59fa118390fcb08e0d53
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-05 14:22:53 +0200

    delete unused methods from use cases

--- app/concepts/reservations/use_cases/create.rb
+++ app/concepts/reservations/use_cases/create.rb
@@ -22,21 +22,8 @@ module Reservations
 
       private
 
-      def create_tickets(reservation)
-        seats.each do |seat|
-          Ticket.create!(seat:, reservation_id: reservation.id)
-        end
-      end
-
       def screening
         Screening.find(screening_id)
-      end
-
-      def reservation_transaction
-        ActiveRecord::Base.transaction do
-          @reservation = reservation
-          create_tickets(@reservation)
-        end
       end
     end
   end
