b6065403794e86ac24cb2d7e52ba0be3df47a17e
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-06 10:46:29 +0200

    update cancel tests

--- spec/use_cases/reservations/cancel_spec.rb
+++ spec/use_cases/reservations/cancel_spec.rb
@@ -2,18 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Reservations::UseCases::Cancel do
   describe '.call' do
+    subject(:cancel_reservation) { described_class.new(id: id).call }
     let(:movie) { create(:movie) }
     let(:hall) { create(:hall) }
     let(:screening) { create(:screening, hall:, movie:, start_time: Date.current + 2.days) }
-    let(:reservation) { create(:reservation, :skip_validate) }
-
-    let(:params) do
-      {
-        id: reservation.id
-      }
-    end
-
-    subject(:cancel_reservation) { described_class.new(**params).call }
+    let(:ticket) { build(:ticket, seat: '1') }
+    let(:reservation) { create(:reservation, tickets: [ticket]) }
+    let(:id) { reservation.id }
 
     it 'cancels reservation' do
       expect { cancel_reservation }.to change { reservation.reload.status }.from('booked').to('cancelled')
--- spec/use_cases/reservations/update_spec.rb
+++ spec/use_cases/reservations/update_spec.rb
@@ -2,21 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Reservations::UseCases::Update do
   describe '.call' do
+    subject(:update_reservation) { described_class.new(id: reservation.id, status: :accepted).call }
     let(:movie) { create(:movie) }
     let(:hall) { create(:hall) }
     let(:screening) { create(:screening, hall:, movie:, start_time: Date.current + 2.days) }
-    let(:reservation) { create(:reservation, :skip_validate) }
-
-    let(:params) do
-      {
-        id: reservation.id,
-        status: :accepted
-
-      }
-    end
-
-    subject(:update_reservation) { described_class.new(**params).call }
-
+    let(:ticket) { build(:ticket, seat: '1') }
+    let(:reservation) { create(:reservation, tickets: [ticket]) }
+   
     it 'updates reservation status' do
       expect { update_reservation }.to change { reservation.reload.status }.from('booked').to('accepted')
     end
