9594b519dfc5d8555cc00ff009c7276320186f12
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-06 10:46:50 +0200

    delete skip validate trait from reservation factory

--- spec/factories/reservation.rb
+++ spec/factories/reservation.rb
@@ -2,8 +2,5 @@ FactoryBot.define do
   factory :reservation do
     screening
     status { 'booked' }
-    trait :skip_validate do
-      to_create { |instance| instance.save(validate: false) }
-    end
   end
 end
