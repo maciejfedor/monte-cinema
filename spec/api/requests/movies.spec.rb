4af3c10d9ac0d06ca3860dcba76b69504436bee3
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-09 17:43:44 +0200

    add api movies-spec

---
+++ spec/api/requests/movies.spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Movies', type: :request do
+  describe 'GET api/v1/movies' do
+
+    it 'returns succesful response' do
+      get api_v1_movies_url
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET api/v1/movies/movie_id' do
+    let(:movie) { create(:movie) }
+
+    before { movie }
+
+    it 'returns succesful response' do
+      get api_v1_movie_url(movie)
+      expect(response).to have_http_status(200)
+    end
+
+    context 'when movie does not exist' do
+
+      it 'returns error' do
+        get api_v1_movie_url(0)
+        expect(response).to have_http_status(:not_found)
+
+      end
+
+    end
+  end
+
+end
