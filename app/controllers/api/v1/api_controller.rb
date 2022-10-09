f623fe749b3c0bbee9f085b434580e62fc828f64
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-09 12:57:33 +0200

    add devise token auth concern to api controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,9 +1,8 @@
 module Api
   module V1
     class ApiController < ActionController::API
+      include DeviseTokenAuth::Concerns::SetUserByToken
       rescue_from ActiveRecord::RecordNotFound, with: :not_found_error
-      protect_from_forgery with: :null_session
-      skip_before_action :verify_authenticity_token
       def not_found_error
         render json: { error: 'Record not found' }, status: :not_found
       end
