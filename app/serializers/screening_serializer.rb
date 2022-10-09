7a1122650990010222d3f2a45b0253be6782f9c7
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-09 16:47:21 +0200

    delete end time from screening serializer

--- app/serializers/screening_serializer.rb
+++ app/serializers/screening_serializer.rb
@@ -2,7 +2,7 @@ class ScreeningSerializer
   include JSONAPI::Serializer
 
   set_type :screening
-  attributes :start_time, :end_time
+  attributes :start_time, :available_seats
   belongs_to :movie
   belongs_to :hall
 end
