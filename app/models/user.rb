a872325270f8e4414f8f73cde2f27e602f6143fc
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-09 16:45:09 +0200

    add user token auth concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  include DeviseTokenAuth::Concerns::User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validate :validate_password_length
@@ -7,6 +8,6 @@ class User < ApplicationRecord
   enum :role, { user: 0, manager: 1, admin: 2 }
 
   def validate_password_length
-    errors.add(:password, :too_long, message: 'Password is too long') if password.nil? || password.bytesize > 72
+    errors.add(:password, :too_long, message: 'Password is too long') if !password.nil? && password.bytesize > 72
   end
 end
