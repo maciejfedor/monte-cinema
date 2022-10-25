7ba29f188935c539c1945ac53935719a46e0102b
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-25 21:43:40 +0200

    correct api movies request spec

--- spec/api/requests/movies_spec.rb
+++ spec/api/requests/movies_spec.rb
@@ -29,6 +29,7 @@ RSpec.describe 'Movies', type: :request do
 
   describe 'GET api/v1/movies/movie_id' do
     let(:movie) { create(:movie) }
+    let(:screening) {create(:screening, movie_id: movie.id)}
     let(:movie_attr) do
       {
         title: movie.title,
@@ -36,7 +37,10 @@ RSpec.describe 'Movies', type: :request do
       }
     end
 
-    before { movie }
+    before do
+      movie
+      screening
+    end
 
     context 'when movie is found' do
       it 'returns succesful response' do
@@ -52,6 +56,13 @@ RSpec.describe 'Movies', type: :request do
       it 'returns correct movie attributes' do
         get api_v1_movie_url(movie)
         expect(JSON.parse(response.body).deep_symbolize_keys[:data][:attributes]).to eq(movie_attr)
+      end
+
+      it 'returns count of screenings for a given movie' do
+        get api_v1_movie_url(movie)
+        debugger
+        expect(JSON.parse(response.body).deep_symbolize_keys
+        [:data][:relationships][:screenings][:meta][:count]).to eq(1)
       end
     end
 
