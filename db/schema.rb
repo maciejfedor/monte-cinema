92cfbf23ba961a933e4ee31b3aa3f01484546559
Maciej Fedor <maciej.fedor@monterail.com>
2022-10-28 11:22:45 +0200

    add deleted at columns to movies and screenings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_09_001450) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_114242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_09_001450) do
     t.integer "duration", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_movies_on_deleted_at"
   end
 
   create_table "reservations", force: :cascade do |t|
@@ -45,6 +47,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_09_001450) do
     t.bigint "hall_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_screenings_on_deleted_at"
     t.index ["hall_id"], name: "index_screenings_on_hall_id"
     t.index ["movie_id"], name: "index_screenings_on_movie_id"
   end
